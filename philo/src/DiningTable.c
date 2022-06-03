44214306dbaef8d2a9b121b0229ab16f33d65636
Abderrahim Boudounit <aboudoun@e1r7p12.1337.ma>
2022-06-03 15:28:28 +0100

    bonus part

---
+++ philo/src/DiningTable.c
@@ -0,0 +1,63 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   DiningTable.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aboudoun <aboudoun@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/25 19:40:15 by aboudoun          #+#    #+#             */
+/*   Updated: 2022/05/30 15:39:13 by aboudoun         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "philosophers.h"
+
+static void	ft_usleep(unsigned long time, unsigned long start)
+{
+	usleep(time * 1000 * 0.95);
+	while (ft_get_time() - start < time)
+		usleep(50);
+}
+
+void	eat(t_philo *ph)
+{
+	ft_print("is eating", ph->nbr + 1, ph->data);
+	ph->last_eat = ft_get_time();
+	ph->nbr_eat++;
+	if (ph->nbr_eat == ph->data->must_eat)
+		ph->data->philo_have_eaten++;
+	ft_usleep((ph->data->time_eat), ft_get_time());
+}
+
+void	ft_sleep(t_philo *ph)
+{
+	ft_print("is sleeping", ph->nbr + 1, ph->data);
+	ft_usleep((ph->data->time_sleep), ft_get_time());
+}
+
+void	take_fork(t_philo *ph)
+{
+	pthread_mutex_lock(&(ph->left_fork));
+	ft_print("is taking a fork", ph->nbr + 1, ph->data);
+	pthread_mutex_lock(ph->right_fork);
+	ft_print("is taking a fork", ph->nbr + 1, ph->data);
+	eat(ph);
+	pthread_mutex_unlock(&(ph->left_fork));
+	pthread_mutex_unlock(ph->right_fork);
+}
+
+void	*ft_actions(void *philo)
+{
+	t_philo	*ph;
+
+	ph = philo;
+	if (ph->nbr % 2 == 0)
+		ft_usleep(100, ft_get_time());
+	while (1)
+	{
+		take_fork(ph);
+		ft_sleep(ph);
+		ft_print("is thinking", ph->nbr + 1, ph->data);
+		usleep(100);
+	}
+}
---
+++ philo_bonus/src/philo_bonus.c
@@ -0,0 +1,65 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aboudoun <aboudoun@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/25 19:37:16 by aboudoun          #+#    #+#             */
+/*   Updated: 2022/05/30 15:38:56 by aboudoun         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "philosophers.h"
+
+int	end_diner(t_data *data)
+{
+	int	i;
+	int	deth;
+
+	i = -1;
+	while (++i <= data->nb_philo)
+	{
+		if (data->philo_have_eaten == data->nb_philo)
+			break ;
+		deth = (ft_get_time() - data->philo[i].last_eat);
+		if (deth >= data->time_die)
+		{
+			data->on_dead = 1;
+			ft_print("died", i + 1, data);
+			break ;
+		}
+		if (i == data->nb_philo - 1)
+			i = -1;
+		usleep(100);
+	}
+	ft_destroy(data);
+	return (0);
+}
+
+void	get_philodata(t_data *data)
+{
+	int	i;
+
+	i = -1;
+	data->start_time = ft_get_time();
+	while (++i < data->nb_philo)
+	{
+		data->philo[i].nbr = i;
+		pthread_mutex_init(&(data->philo[i].left_fork), NULL);
+		data->philo[i].data = data;
+	}
+	i = -1;
+	while (++i < data->nb_philo)
+	{
+		data->philo[i].nbr_eat = 0;
+		data->philo[i].last_eat = ft_get_time();
+		if (i == data->nb_philo - 1)
+			data->philo[i].right_fork = &(data->philo[0].left_fork);
+		else
+			data->philo[i].right_fork = &(data->philo[i + 1].left_fork);
+		pthread_create(&(data->philo->philo_t), \
+				NULL, &ft_actions, &data->philo[i]);
+		pthread_detach(data->philo->philo_t);
+	}
+}
